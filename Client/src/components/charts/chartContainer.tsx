598aa0fd4917750108b1c13955056faabe1761f7
Pablo Risi <pablomrisi@hotmail.com>
2026-02-24 19:14:28 -0300

    component charts

--- Client/src/components/charts/chartContainer.tsx
+++ Client/src/components/charts/chartContainer.tsx
@@ -5,26 +5,25 @@ import { useTasks } from "../../context/taskContex"
 
 export const ChartContainer = () => {
 
-  const{task, getCompletedTask, getFailTask,getPendigTask} = useTasks()
+  const{ 
+    getCompletedTask,
+    getCompletedSubtasks,
+    getFailTask,
+    getPendigTask, 
+    getPendingSubtasks
+     
+    } = useTasks()
 
   const completedMain = getCompletedTask()
+  const subTaskCompleted = getCompletedSubtasks()
   const failTask = getFailTask()
   const pendigTask = getPendigTask()
+  const pendingsubtask = getPendingSubtasks()
 
   
 
   return (
     <div className="task-table-container">
-      
-      {
-        task.map(tas => (
-          <ul key={tas._id}>
-            <li>
-              
-            </li>
-          </ul>
-        ))
-      }
 
       <span> tareas Completadas:
         {
@@ -37,10 +36,31 @@ export const ChartContainer = () => {
             </li>
           )) 
         }  
+        {
+                subTaskCompleted.map((task,id) => (
+                  <li key={id}>
+                          <span className="task-title-inline">{task.title}</span>
+                        <span className={`task-priority-inline priority-${task.priority.toLowerCase()}`}>
+                                                                {task.priority}
+                        </span>
+                  </li>
+                )) 
+        }
       </span>
       <span> tareas Pendientes:
         {
           pendigTask.map((task,idx )=> (
+            <li key={idx}>
+                    <span className="task-title-inline">{task.title}</span>
+                  <span className={`task-priority-inline priority-${task.priority.toLowerCase()}`}>
+                                                          {task.priority}
+                  </span>
+            </li>
+          )) 
+        }  
+
+      {
+          pendingsubtask.map((task,idx )=> (
             <li key={idx}>
                     <span className="task-title-inline">{task.title}</span>
                   <span className={`task-priority-inline priority-${task.priority.toLowerCase()}`}>
